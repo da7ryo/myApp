d4357ff95cfd79813d32be4d578f139d70434cb4
Dario Milanović <da7ryo@gmail.com>
2026-02-13 20:06:56 +0100

    Introducing into patch method

--- server.js
+++ server.js
@@ -1,23 +1,66 @@
 const fs = require("fs");
 const express = require("express");
+const { randomUUID } = require("crypto"); // vraca string, jedinstveni id za svaki poziv
 
 const app = express();
 app.use(express.json());
 
 // const homeHtml = fs.readFileSync("./home.html", "utf-8");
 
-const users = [];
+let animals = [];
 
-app.get("/", function (req, res) {
-  res.json(users);
+app.get("/animals", function (req, res) {
+  res.json(animals);
 });
 
-app.post("/", function (req, res) {
-  const newUser = req.body;
-  users.push(newUser);
+app.post("/animals", function (req, res) {
+  const newAnimal = {
+    ...req.body,
+    id: randomUUID(),
+  };
 
-  res.json(newUser);
+  animals.push(newAnimal);
+
+  res.json(newAnimal);
 });
+
+app.get("/animals/:id", function (req, res) {
+  const { id } = req.params;
+  const selectedAnimal = animals.find((animal) => animal.id === id);
+
+  if (!selectedAnimal) {
+    return res.json({ msg: "Animal not found" });
+  }
+
+  res.json(selectedAnimal);
+});
+
+app.patch("/animals/:id", function (req, res) {
+  const { id } = req.params;
+  const selectedAnimal = animals.find((animal) => animal.id === id);
+
+  if (!selectedAnimal) {
+    return res.json({ msg: "Animal not found" });
+  }
+
+  const { name } = req.body;
+  animals = animals.map((animal) => {
+    if (animal.id === id) {
+      return { ...animal, name };
+    } else {
+      return animal;
+    }
+  });
+
+  const updatedAnimal = animals.find((animal) => animal.id === id);
+
+  console.log(updatedAnimal);
+
+  res.json(updatedAnimal);
+});
+// dobiti u req.body name property, u req.params cu dobiti id, trebam u animals arrayu naci zeljeni objekt i postaviti mu
+// ime na vrijednost koju sam dobio u req.body i vratiti promijenjeni objekt, uz koristenje map metode trebam update
+// uraditi
 
 app.listen(8080, function () {
   console.log("Hello from the server");
